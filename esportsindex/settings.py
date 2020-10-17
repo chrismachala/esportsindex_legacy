8c56c2cf9771e0ebc330bbc707d78f360050c874
Chris Machala <chrismachala@outlook.com>
2020-10-17 13:17:26 +0100

    User Registration / Login app and html resources

--- esportsindex/settings.py
+++ esportsindex/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',s
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
