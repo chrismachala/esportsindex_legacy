e43dcaa2250b4dd818eb608e4509b7327eca5b65
Chris Machala <chrismachala@outlook.com>
2020-10-18 16:44:27 +0100

    v1 pricing engine

--- esi_players/views.py
+++ esi_players/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from esi_players.models import Player, Team, PlayerTeam
 from django.http import HttpResponse
-
+from esi_pricingengine.models import Asset
 
 # Create your views here.
 
@@ -13,18 +13,18 @@ def index(request):
 
 def players(request):
     template_name = 'esi_players/players.html'
-    players_list = Player.objects.all()
+    asset_list = Asset.objects.all()
 
     template = "esi_players/base_generic.html"
 
     return render(request, template_name,
-                  {'players': players_list, 'template': template})
+                  { 'assets': asset_list, 'template': template})
 
 
 def player(request, player_pk):
 
     template = "esi_players/base_generic.html"
-
+    asset = Asset.objects.get(player_id=player_pk)
     player_ins = Player.objects.filter(id=player_pk).first()
     team = PlayerTeam.objects.filter(player__player_id=player_ins.player_id).first().team
     template_name = 'esi_players/player.html'
@@ -34,5 +34,5 @@ def player(request, player_pk):
         if pt.player.player_id != player_ins.player_id:
             team_list.append(pt.player)
 
-    return render(request, template_name, {'player': player_ins, 'team': team, 'team_list':team_list, 'template':template})
+    return render(request, template_name, {'player': player_ins,'asset':asset, 'team': team, 'team_list':team_list, 'template':template})
 
