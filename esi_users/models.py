c9dab8fde9d2c4e4eae12efbcfbfbfb940c10819
Chris Machala <chrismachala@outlook.com>
2020-11-07 13:01:24 +0000

    removed username from model Profile

--- esi_users/models.py
+++ esi_users/models.py
@@ -8,7 +8,6 @@ from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    username = models.CharField(max_length=200, null=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=150)
